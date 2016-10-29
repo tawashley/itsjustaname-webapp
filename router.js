8765757efbe907ceefc0b7c6e069d39e066535f1
tawashley <trisashley@gmail.com>
2016-10-29 22:47:31 +0100

    refactor: remove commented code

--- router.js
+++ router.js
@@ -6,13 +6,11 @@ var upload = multer({dest: 'uploads'});
 module.exports = function(app) {
 
 	var homeController = require('./controllers/home');
-	// var sitemapController = require('./controllers/sitemap');
 	var pageNotFoundController = require('./controllers/page-not-found');
 	var errorController = require('./controllers/server-error');
 
 	app.get('/page-not-found', pageNotFoundController.get);
 	app.get('/error', errorController.get);
-	// app.get('/sitemap.xml', sitemapController.get);
 	app.get('/', homeController.get);
 
 	app.use(function(request, response) {
